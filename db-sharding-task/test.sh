5aa8a19abd21a2d4cddc7e6663483e13e78aedf2
RomRom <imperator198@gmail.com>
2022-01-23 19:01:55 +0200

    update script

--- db-sharding-task/test.sh
+++ db-sharding-task/test.sh
@@ -5,10 +5,10 @@ docker exec postgres-b psql -U postgres -d task -c "delete from books_1;"
 docker exec postgres-b psql -U postgres -d task -c "delete from books_2;"
 docker exec postgres-sa psql -U postgres -d task -c "delete from books;"
 
-time docker exec postgres-b psql -U postgres -d task -c "call populate(10000);"
+time docker exec postgres-b psql -U postgres -d task -c "call populate(100000);"
 
 docker exec postgres-b psql -U postgres -d task -c "select count(*) from books;"
 
-time docker exec postgres-sa psql -U postgres -d task -c "call populate(10000);"
+time docker exec postgres-sa psql -U postgres -d task -c "call populate(100000);"
 
 docker exec postgres-sa psql -U postgres -d task -c "select count(*) from books;"
